32e0188b417588da30cfe733cacc87a7a75abeba
Pierre Narvor <pnarvor@laas.fr>
2019-03-06 11:16:13 +0100

    [metcdf] Updated mesonh_probe_test

--- tests/mesonh_probe_test.py
+++ tests/mesonh_probe_test.py
@@ -20,13 +20,20 @@ test file for periodiccontainer and netcdfinterface types
 mesonhfiles = sys.argv[slice(1,len(sys.argv))]
 atm = MFDataset(mesonhfiles)
 
-probe = cdf.MesoNHProbe(atm, ['RCT', 'THT'], [10,20,20,20], initPosition=[10,20,20,20])
+# probe = cdf.MesoNHProbe(atm, ['RCT', 'THT', 'UT', 'VT', 'WT'], [10,20,40,40], initPosition=[200,80,200,200])
+probe = cdf.MesoNHProbe(atm, ['RCT', 'THT'], [10,20,40,40], initPosition=[200,80,200,200])
+# probe = cdf.MesoNHProbe(atm, ['RCT', 'WT'], [20,20,40,40], initPosition=[10,20,20,20])
 # probe = cdf.MesoNHProbe(atm, ['RCT'], [10,20,20,20], initPosition=[10,20,20,20])
 
-for i in range(30):
+lastTime = time.time()
+for i in range(300):
     # print(probe[10,20,20,20 + i],"\n\n\n")
-    print(probe[10,20,20,20 + i],"\n")
+    tmp = probe[200 + i,80,200+i,200 + 2*i]
+    # print(tmp,"\n")
     time.sleep(0.5)
+    nextTime = time.time()
+    print("Ellapsed : ", nextTime - lastTime)
+    lastTime = nextTime
 
 
 
