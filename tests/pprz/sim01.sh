58a7a8cb0f6a2d37471bb4150a98cbb990d66f73
Pierre Narvor <pnarvor@laas.fr>
2019-03-25 16:00:52 +0400

    [sim] Added a script to launch a basic sim with 2 UAVs

---
+++ tests/pprz/sim01.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_0 -t sim --boot --norc & sleep 1 &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_1 -t sim --boot --norc & sleep 1 &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/ground_segment/cockpit/gcs -layout large_left_col.xml &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/ground_segment/tmtc/server -n
+
+
