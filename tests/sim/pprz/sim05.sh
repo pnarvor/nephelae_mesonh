e823bb9a0cae42fb8373b2bf02e2141b63c1cbb2
Pierre Narvor <pnarvor@laas.fr>
2019-04-15 12:20:53 +0200

    [sim] Added a script to launch several pprz uav sim in one call

---
+++ tests/sim/pprz/sim05.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_0 -t sim --boot --norc &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_1 -t sim --boot --norc &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_2 -t sim --boot --norc &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_3 -t sim --boot --norc &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_4 -t sim --boot --norc &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/ground_segment/cockpit/gcs -layout large_left_col.xml &
+/home/pnarvor/work/nephelae/soft/paparazzi/sw/ground_segment/tmtc/server -n
+
+
