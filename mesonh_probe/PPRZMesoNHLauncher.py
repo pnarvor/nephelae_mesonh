74a05acbd9748e246df5adf77f76c0226f3db10f
Pierre Narvor <pnarvor@laas.fr>
2019-04-25 16:50:10 +0200

    [sim] Adapted PPRZMesoNHLauncher to WORLD_ENV messages

--- mesonh_probe/PPRZMesoNHLauncher.py
+++ mesonh_probe/PPRZMesoNHLauncher.py
@@ -3,13 +3,18 @@ import sys
 import sh
 import numpy as np
 import signal
+import utm
+import time
 
 from .Fancy       import Fancy
 
-PPRZ_HOME = os.getenv("PAPARAZZI_HOME")
-sys.path.append(PPRZ_HOME + "/var/lib/python")
-from pprzlink.ivy import IvyMessagesInterface
-from pprzlink.message import PprzMessage
+# PPRZ_HOME = os.getenv("PAPARAZZI_HOME")
+# sys.path.append(PPRZ_HOME + "/var/lib/python")
+# from pprzlink.ivy import IvyMessagesInterface
+# from pprzlink.message import PprzMessage
+
+from ivy.std_api import *
+import logging
 
 class PPRZMesoNHLauncher:
 
@@ -23,12 +28,20 @@ class PPRZMesoNHLauncher:
         self.uavT0 = -1.0
         self.uavIDs = []
 
-        self.ivy = IvyMessagesInterface("MesoNHSensors_" + str(os.getpid()))
-        self.ivy.subscribe(self.find_uavs_callback,'(.* GPS .*)')
+        # self.ivy = IvyMessagesInterface("MesoNHSensors_" + str(os.getpid()))
+        # self.ivy.subscribe(self.find_uavs_callback,'(.* GPS .*)')
+        # self.ivy.subscribe(self.find_uavs_callback_world,'(.* WORLD_ENV_REQ .*)')
+
+        ivy = IvyInit("MesoNHSensors_" + str(os.getpid()))
+        # set log level to hide INFO stdout messages
+        logging.getLogger('Ivy').setLevel(logging.WARN) 
+        IvyStart("127.255.255.255:2010")
+        IvyBindMsg(lambda agent, msg: self.find_uavs_callback_world(agent, msg),
+                   '(.* WORLD_ENV_REQ .*)')
 
     def stop(self):
         print("\nShutting down...")
-        self.ivy.shutdown()
+        # self.ivy.shutdown()
         for p in self.probes:
             p.signal(signal.SIGINT)
         print("Done !")
@@ -44,7 +57,21 @@ class PPRZMesoNHLauncher:
         if not ivyAgent in self.uavIDs:
             print("Found UAV : ", ivyAgent)
             self.uavIDs.append(ivyAgent)
-            self.launch_probe(ivyAgent)
+            # self.launch_probe(ivyAgent)
+
+    def find_uavs_callback_world(self, ivyAgent, msg):
+
+        # print("Agent : ", ivyAgent, " sent : ", msg)
+
+        if self.uavT0 < 0.0:
+            self.uavT0 = time.time() # not perfect but neither is pprz
+            print("Setting t0 : ", self.uavT0)
+
+        uavId = int(msg.split()[1].split("_")[0])
+        if not uavId in self.uavIDs:
+            print("Found UAV : ", uavId)
+            self.uavIDs.append(uavId)
+            # self.launch_probe(uavId)
 
     def launch_probe(self, uavID):
         command = sh.Command(self.probeExePath)
