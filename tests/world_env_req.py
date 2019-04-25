efb31641745b97f582df05eeeb73eaefc4ecef0a
Pierre Narvor <pnarvor@laas.fr>
2019-04-25 16:31:50 +0200

    [sim] Tranlated wind request gps to UTM

--- tests/world_env_req.py
+++ tests/world_env_req.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import signal
+import utm
 
 def signal_handler(signal, frame):
     print("Shutdown...")
@@ -28,14 +29,17 @@ logging.getLogger('Ivy').setLevel(logging.WARN) # set log level to hide INFO std
 IvyStart("127.255.255.255:2010")                # IvyBus on localhost
 
 def callback_01(agent, msg):
-    # print("Agent \"" + str(agent) + "\" sent : \"" + str(msg) + "\"")
+    print("Agent \"" + str(agent) + "\" sent : \"" + str(msg) + "\"")
     values = str(msg).split(" ")
+    utmPos = utm.from_latlon(float(values[3]), float(values[4]))
+    print("UTM : ", utmPos)
     response = (values[1] + " " + values[0] + " WORLD_ENV "
                 + str(UT) + " " + str(VT) + " " + str(WT) + " 266.0 1.0 1")
     # print("Response : \"" + response + "\"")
     IvySendMsg(response)
 
-IvyBindMsg(callback_01, '(.* 17938_.* WORLD_ENV_REQ .*)')
+IvyBindMsg(callback_01, '(.* 23244_.* WORLD_ENV_REQ .*)')
+# IvyBindMsg(callback_01, '(.* WORLD_ENV_REQ .*)')
 
 # IvyBus related code stops here #################################
 
