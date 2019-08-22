2ace4848e61fa02a4ca520af7601cdbada6a7a03
Pierre Narvor <pnarvor@laas.fr>
2019-08-22 19:26:17 +0200

    [refactor] Removed unused files (some were moved in nephelae_core repository)

--- mesonh_interface/__init__.py
+++
@@ -1,9 +0,0 @@
-from .DimensionHelper   import DimensionBounds
-from .DimensionHelper   import DimensionHelper
-from .ScaledArray       import ScaledArray
-from .NetCDFInterface   import NetCDFInterface
-from .PeriodicContainer import PeriodicContainer
-from .MesoNHVariable    import MesoNHVariable
-
-
-
--- mesonh_probe/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Fancy                 import Fancy
-from .MesoNHCachedProbe     import MesoNHCachedProbe
-from .PPRZMesoNHInterface   import PPRZMesoNHInterface
-from .PPRZMesoNHLauncher    import PPRZMesoNHLauncher
