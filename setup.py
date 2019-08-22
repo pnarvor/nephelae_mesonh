6518157c92ad6ef189d8e099dcdf1ed59253acdc
Pierre Narvor <pnarvor@laas.fr>
2019-08-22 19:29:26 +0200

    [refactor] Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='nephelae_mesonh',
+      version='0.1',
+      description='MesoNH utilities for nephelae_project',
+      url='ssh://git@redmine.laas.fr/laas/users/simon/nephelae/nephelae-devel/nephelae_simulation.git',
+      author='Pierre Narvor',
+      author_email='pnarvor@laas.fr',
+      licence='bsd3',
+      packages=find_packages(include=['nephelae_mesonh']),
+      install_requires=[
+        'numpy',
+        'netCDF4',
+        'utm',
+        'matplotlib',
+        'sh'
+      ],
+      zip_safe=False)
+
+
