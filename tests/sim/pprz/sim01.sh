aae3f00e225c606977757383518b8e62aa90ed56
Pierre Narvor <pnarvor@laas.fr>
2019-08-27 14:29:21 +0200

    [refactor] Removed deprecated tests

--- tests/sim/pprz/sim01.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-$PAPARAZZI_HOME/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_0 -t sim --boot --norc &
-$PAPARAZZI_HOME/sw/simulator/pprzsim-launch -b 127.255.255.255 -a Microjet_neph_1 -t sim --boot --norc &
-$PAPARAZZI_HOME/sw/ground_segment/cockpit/gcs -layout large_left_col.xml &
-$PAPARAZZI_HOME/sw/ground_segment/tmtc/server -n
-
-
