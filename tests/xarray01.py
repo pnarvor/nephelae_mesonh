6fc135e6dabd5852edc25831cdc3e281186fe55a
Pierre Narvor <pnarvor@laas.fr>
2019-10-10 10:34:23 +0200

    Tested new mesonh datasxet (some tests are not updated and therefore might be broken)

--- tests/xarray01.py
+++ tests/xarray01.py
@@ -10,8 +10,11 @@ import numpy  as np
 from netCDF4 import MFDataset
 
 # mesonhFiles = '/home/pnarvor/work/nephelae/data/MesoNH-2019-02/REFHR.1.ARMCu.4D.nc'
-mesonhFiles = glob('/home/pnarvor/work/nephelae/data/nephelae-remote/Nephelae_tmp/download/L12zo.1.BOMEX.OUT.*.nc')
-mesonhFiles.sort()
+# mesonhFiles = glob('/home/pnarvor/work/nephelae/data/nephelae-remote/Nephelae_tmp/download/L12zo.1.BOMEX.OUT.*.nc')
+# mesonhFiles.sort()
+mesonhFiles = '/home/pnarvor/work/nephelae/data/nephelae-remote/MesoNH02/bomex_hf.nc'
+# mesonhFiles = ['/home/pnarvor/work/nephelae/data/nephelae-remote/MesoNH02/bomex_hf.nc']
+
 
 atm0 = xr.open_mfdataset(mesonhFiles)
 rct0  = atm0.RCT
